dbdec0c18002368500a37500152554ea5cce52af
jiri.pesik <jiri.pesik@outlook.cz>
2022-10-19 18:39:45 +0200

    Oprava

--- program.py
+++ program.py
@@ -1,6 +1,11 @@
 import json
+import requests
 
-with open("zavod.json", encoding="utf-8") as soubor:
-    zavod = json.load(soubor)
-
-print(zavod[0])
+response = requests.get("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
+populace = response.text
+populace = json.loads(populace)
+vystup = {}
+for row in populace["data"]:
+    vystup[row["Year"]] = row["Population"]
+with open("population_usa.json", mode="w", encoding="utf-8") as soubor:
+    json.dump(vystup, soubor, indent=4)
